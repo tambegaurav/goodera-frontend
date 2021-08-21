dc2255835835f5df0066033bd3a91f82500bad02
Gaurav <gauravkishortambe@gmail.com>
2021-08-21 16:42:32 +0530

    project init and add navbar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  width: 100%;
+  height: 200px;
+  background: rgb(94, 122, 223);
+  background: linear-gradient(
+    90deg,
+    rgba(94, 122, 223, 1) 22%,
+    rgba(94, 169, 223, 1) 100%
+  );
+`;
+
+const NavBarWrapper = styled.div`
+  display: flex;
+  width: 70%;
+  height: 100px;
+  margin: auto;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+
+  & .left-links,
+  .right-links {
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+    color: #f5f5f5;
+    cursor: pointer;
+  }
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  width: 70%;
+  margin: auto;
+  color: #f5f5f5;
+
+  & h1 {
+    font-weight: 700;
+    font-size: 35px;
+  }
+`;
+
+const SearchBar = styled.input``;
+
+const NavBar = () => {
+  return (
+    <MainWrapper>
+      <NavBarWrapper>
+        <div className="left-links">
+          <img src="/images/Logo.png" className="logo" alt="" />
+
+          <h3>Find Jobs</h3>
+          <h3>Upskill Yourself</h3>
+        </div>
+
+        <div className="right-links">
+          <h3>Post A Job</h3>
+          <h3>Sign in</h3>
+        </div>
+      </NavBarWrapper>
+      <Title>
+        <h1>Find Your Dream Job</h1>
+        <p>Browse through thousands of full-time and part-time jobs near you</p>
+      </Title>
+      {/* <SearchBar></SearchBar> */}
+    </MainWrapper>
+  );
+};
+
+export default NavBar;
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../Components/NavBar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default HomePage;
